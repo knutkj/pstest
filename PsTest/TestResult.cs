65f77f12d0c9b08eb8fcf78c51cc78708387c8c2
Knut Kristian Johansen <knutkj@gmail.com>
2012-09-20 18:33:16 +0200

    Implemented a simple TestResult class.

---
+++ PsTest/TestResult.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace PsTest
+{
+    /// <summary>
+    /// Represents the result of an invoked unit test.
+    /// </summary>
+    public class TestResult
+    {
+        private readonly string _testName;
+        private readonly bool _success;
+
+        /// <summary>
+        /// Initializes a new instance of the TestResult class with the
+        /// specified test name and success status.
+        /// </summary>
+        /// <param name="testName">
+        /// The name of the test.
+        /// </param>
+        /// <param name="success">
+        /// True if the test was a success or false.
+        /// </param>
+        public TestResult(string testName, bool success)
+        {
+            if (testName == null)
+            {
+                throw new ArgumentNullException("testName");
+            }
+            _testName = testName;
+            _success = success;
+        }
+
+        /// <summary>
+        /// Get the name of the test.
+        /// </summary>
+        public string TestName { get { return _testName; } }
+
+        /// <summary>
+        /// Get a value indicating if the test was successful or not.
+        /// </summary>
+        public bool Success { get { return _success; } }
+    }
+}
---
+++ PsTestTests/TestResultTests.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PsTest;
+using System;
+
+namespace PsTestTests
+{
+    [TestClass]
+    public class TestResultTests
+    {
+        [TestMethod, ExpectedException(typeof(ArgumentNullException))]
+        public void CtorNoTestNameArgumentNullException()
+        {
+            // Arrange.
+
+            // Act and assert.
+            new TestResult(null, false);
+        }
+
+        [TestMethod]
+        public void CtorSavesArgs()
+        {
+            // Arrange.
+            const string testName = "test name";
+            const bool success = true;
+
+            // Act.
+            var res = new TestResult(testName, success);
+
+            // Assert.
+            Assert.AreEqual(testName, res.TestName);
+            Assert.AreEqual(success, res.Success);
+        }
+    }
+}
